6c9db63f8d89fb7d23940617cf432a6f1a13410b
J. Cardenas <jonscardenas@gmail.com>
2025-07-23 21:35:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,24 @@
-<script>
-  // Get the elements
-    const option1 = document.getElementById('option1');
-    const option2 = document.getElementById('option2');
-    const dateContainer = document.getElementById('dateContainer');
+// Get the elements
+  const option1 = document.getElementById('option1');
+  const option2 = document.getElementById('option2');
+  const dateContainer = document.getElementById('dateContainer');
 
-    option1.addEventListener('click', (e) => {
-      //e.preventDefault();
-      option1.checked = true;
-      option2.checked = false;
-      option1.setAttribute('aria-checked', 'true');
-      option2.setAttribute('aria-checked', 'false');
-      dateContainer.classList.add('visuallyhidden');
-      dateContainer.setAttribute('aria-hidden', 'true');
-    });
+  option1.addEventListener('click', (e) => {
+    //e.preventDefault();
+    option1.checked = true;
+    option2.checked = false;
+    option1.setAttribute('aria-checked', 'true');
+    option2.setAttribute('aria-checked', 'false');
+    dateContainer.classList.add('visuallyhidden');
+    dateContainer.setAttribute('aria-hidden', 'true');
+  });
 
-    option2.addEventListener('click', (e) => {
-      //e.preventDefault();
-      option2.checked = true;
-      option1.checked = false;
-      option2.setAttribute('aria-checked', 'true');
-      option1.setAttribute('aria-checked', 'false');
-      dateContainer.classList.remove('visuallyhidden');
-      dateContainer.setAttribute('aria-hidden', 'false');
-    });
-
-</script>
+  option2.addEventListener('click', (e) => {
+    //e.preventDefault();
+    option2.checked = true;
+    option1.checked = false;
+    option2.setAttribute('aria-checked', 'true');
+    option1.setAttribute('aria-checked', 'false');
+    dateContainer.classList.remove('visuallyhidden');
+    dateContainer.setAttribute('aria-hidden', 'false');
+  });
